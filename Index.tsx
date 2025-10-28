097c56d25336026483126372d10a90cc9d94a694
hemalvarasada1999 <167778748+hemalvarasada1999@users.noreply.github.com>
2025-10-28 11:48:57 +0530

    added footer and change the title

---
+++ Index.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/Hero";
+import ExpertSection from "@/components/ExpertSection";
+import TrustSection from "@/components/TrustSection";
+import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen">
+      <Hero />
+      <ExpertSection />
+      <TrustSection />
+      <FinalCTA />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
